b96ab6cad549355970853d1e7caca2ec1fcb842b
kimwook123 <139030617+kimwook123@users.noreply.github.com>
2023-11-22 19:21:44 +0900

    초기 커밋.

---
+++ MFCApplication3/MFCApplication3Dlg.cpp
@@ -0,0 +1,246 @@
+﻿
+// MFCApplication3Dlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MFCApplication3.h"
+#include "MFCApplication3Dlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+CString ip = _T("10.10.21.117");
+UINT Port = 25500;
+
+// 응용 프로그램 정보에 사용되는 CAboutDlg 대화 상자입니다.
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+// 구현입니다.
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CMFCApplication3Dlg 대화 상자
+
+
+
+CMFCApplication3Dlg::CMFCApplication3Dlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MFCAPPLICATION3_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+
+	if (!m_clientSocket.Create())
+	{
+		AfxMessageBox(L"소켓을 생성할 수 없습니다.");
+		return;
+	}
+}
+
+void CMFCApplication3Dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CMFCApplication3Dlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_BUTTON1, &CMFCApplication3Dlg::OnBnClickedButton1)
+END_MESSAGE_MAP()
+
+
+// CMFCApplication3Dlg 메시지 처리기
+
+BOOL CMFCApplication3Dlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 시스템 메뉴에 "정보..." 메뉴 항목을 추가합니다.
+
+	// IDM_ABOUTBOX는 시스템 명령 범위에 있어야 합니다.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 이 대화 상자의 아이콘을 설정합니다.  응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
+	//  프레임워크가 이 작업을 자동으로 수행합니다.
+	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
+	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
+
+
+
+	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+
+	// 서버에 연결
+	if (!m_clientSocket.Connect(_T("10.10.21.117"), 25000))
+	{
+		AfxMessageBox(_T("서버에 연결할 수 없습니다."));
+		return FALSE;
+	}
+	else
+	{
+		AfxMessageBox(_T("서버에 연결되었습니다."));
+	}
+
+	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+}
+
+void CMFCApplication3Dlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
+//  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 애플리케이션의 경우에는
+//  프레임워크에서 이 작업을 자동으로 수행합니다.
+
+void CMFCApplication3Dlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 아이콘을 그립니다.
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
+//  이 함수를 호출합니다.
+HCURSOR CMFCApplication3Dlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CMFCApplication3Dlg::OnBnClickedButton1()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	CFileDialog dlg(TRUE);
+	if (dlg.DoModal() == IDOK)
+	{
+		CString filePath = dlg.GetPathName();
+		SendImageToServer(filePath);
+	}
+}
+
+
+void CMFCApplication3Dlg::SendImageToServer(const CString& filePath)
+{
+	// 이미지 파일 열기
+	CFile file;
+	if (!file.Open(filePath, CFile::modeRead))
+	{
+		AfxMessageBox(L"이미지 파일을 열 수 없습니다.");
+		return;
+	}
+
+	// 이미지 파일 크기 가져오기
+	ULONGLONG fileSize = file.GetLength();
+
+	// 이미지 파일 크기를 서버에 전송
+	int byteSize = m_clientSocket.Send(&fileSize, sizeof(fileSize));
+	if (byteSize == -1)
+	{
+		int error = m_clientSocket.GetLastError();
+		AfxMessageBox(_T("이미지 파일 크기 전송 실패!"));
+		return;
+	}
+	else
+	{
+		AfxMessageBox(_T("이미지 파일 크기 전송 성공!"));
+	}
+
+	// 이미지 파일 데이터를 서버에 전송
+	BYTE* imageData = new BYTE[fileSize];
+	file.Read(imageData, fileSize);
+	int byte = m_clientSocket.Send(imageData, fileSize);
+
+	if (byte > 0)
+		AfxMessageBox(_T("메시지 전송 완료!"));
+	else if (byte == -1)
+	{
+		int error = m_clientSocket.GetLastError();
+
+
+		AfxMessageBox(_T("메시지 전송 실패!"));
+	}
+		
+	delete[] imageData;
+	file.Close();
+
+	char buffer[1024];
+	int bytesRead = m_clientSocket.Receive(buffer, sizeof(buffer));
+	if (bytesRead > 0)
+	{
+		AfxMessageBox(_T("텍스트 수신 성공!"));
+
+		CString receivedMessage(buffer, bytesRead);
+		AfxMessageBox(receivedMessage);
+	}
+}
